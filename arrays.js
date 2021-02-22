7359589238d166707ecd49b9c2e5883f903f7bfc
Wesley B. Howard <bryce9643@gmail.com>
2021-02-22 02:29:23 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,14 @@
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"
+]
+
+function addElementToEndOfArray(array, element) {
+  chocolateBars.unshift(element)
+}
+
+function addElementToBeginningOfArray(array, element) {
+  chocolateBars.push(element)
+}
